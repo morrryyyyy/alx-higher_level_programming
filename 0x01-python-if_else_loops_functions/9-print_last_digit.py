c27772deed83c8915cbaebcf84e3b8041adcd83b
Maryam <morrryyyy21@gmail.com>
2024-04-03 17:20:23 +0100

    fix pycodestyle error

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,5 +2,5 @@
 def print_last_digit(number):
     abs_number = abs(number)
     last = abs_number % 10
-    print("{}".format(last), end = '')
+    print("{}".format(last), end='')
     return last
