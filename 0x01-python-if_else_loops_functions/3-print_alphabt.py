38b0865105849a7080e6d534b07529df13ec930b
Maryam <morrryyyy21@gmail.com>
2024-04-03 16:50:36 +0100

    print the alphabet in lowercase excluding some letters

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    if chr(i) == 'q' or chr(i) == 'e':
-        continue
-    print(chr(i).format(i), end='')
+    if i == 101 or i == 113:
+        pass
+    else:
+        print("{}".format(chr(i)), end="")
