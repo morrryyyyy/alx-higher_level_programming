22059ff56f8d93ce89bf6d7e85ca1d9616793981
Maryam <morrryyyy21@gmail.com>
2024-04-18 10:04:17 +0100

    fix logical error

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    first = sentence[0]
-    for c in sentence:
-        if length == 0:
-            first = None
-        new_tuple = (length, first)
+    first = sentence[0] if length > 0 else None
+    new_tuple = (length, first)
     return new_tuple
