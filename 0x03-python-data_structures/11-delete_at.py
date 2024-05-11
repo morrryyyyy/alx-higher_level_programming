aba6b695d4c2a893e4c4eebf3ca09e6af3aab321
Maryam <morrryyyy21@gmail.com>
2024-05-11 07:51:19 +0100

    de;ete an element in a list

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,7 +3,7 @@
 def delete_at(my_list=[], idx=0):
     # Handle invalid index
     if idx < 0 or idx > len(my_list):
-        return y_list
+        return my_list
     # Delete item at index
     del my_list[idx]
     return my_list
