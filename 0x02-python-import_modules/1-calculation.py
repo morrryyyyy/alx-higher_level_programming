cd3349fab19c3dcf8cbaefc046f44e69a2d76171
Maryam <morrryyyy21@gmail.com>
2024-05-18 23:47:26 +0100

    add execution exception

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
-a = 10
-b = 5
-sum = add(a, b)
-diff = sub(a, b)
-prod = mul(a, b)
-quo = div(a, b)
+if __name__ == '__main__':
+    a = 10
+    b = 5
+    sum = add(a, b)
+    diff = sub(a, b)
+    prod = mul(a, b)
+    quo = div(a, b)
 
-print("{0} + {1} = {2}".format(a, b, sum)
-print("{0} - {1} = {2}".format(a, b, diff)
-print("{0} * {1} = {2}".format(a, b, prod)
-print("{0} / {1} = {2}".format(a, b, quo)
+    print("{0} + {1} = {2}".format(a, b, sum))
+    print("{0} - {1} = {2}".format(a, b, diff))
+    print("{0} * {1} = {2}".format(a, b, prod))
+    print("{0} / {1} = {2}".format(a, b, quo))
