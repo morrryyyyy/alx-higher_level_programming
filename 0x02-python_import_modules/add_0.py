54b19b810b106a83697640eb44db19c7216115d1
Maryam <morrryyyy21@gmail.com>
2024-05-18 23:03:13 +0100

    correction

--- 0x02-python_import_modules/add_0.py
+++ 0x02-python_import_modules/add_0.py
@@ -10,12 +10,3 @@ def add(a, b):
         The return value. a + b
     """
     return (a + b)
-def main():
-    from add_0 import add
-    if __name__ == '__main__':
-        a = 1
-        b = 2
-        result = add(a, b)
-        print(f"{a} + {b} = {result}")
-if __name__ == '__main__':
-    main()
