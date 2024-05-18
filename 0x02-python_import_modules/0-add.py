25d17b3d92d5926f3766e52bd3bd34d0cc316172
Maryam <morrryyyy21@gmail.com>
2024-05-18 22:59:22 +0100

    add two numbers and print the result

---
+++ 0x02-python_import_modules/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def main():
+    from add_0 import add
+    if __name__ == '__main__':
+        a = 1
+        b = 2
+        result = add(a, b)
+        print(f"{a} + {b} = {result}")
+if __name__ == '__main__':
+    main()
