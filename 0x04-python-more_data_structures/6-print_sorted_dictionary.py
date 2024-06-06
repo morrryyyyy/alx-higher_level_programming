525dcdc80f91bc7daa0a76451621f26d06f55622
Maryam <morrryyyy21@gmail.com>
2024-06-06 12:58:44 +0100

    print a dictinary

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary={}):
+    new_list = list()
+    new_list = sorted(a_dictionary.keys())
+    for item in new_list:
+        value = a_dictionary[item]
+        print(f'{item}:{value}')
