d935adbaacaf9db7355a3b6ecd9f0bf6967f1a4b
Maryam <morrryyyy21@gmail.com>
2024-06-06 12:59:54 +0100

    add space between key and value

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -4,4 +4,4 @@ def print_sorted_dictionary(a_dictionary={}):
     new_list = sorted(a_dictionary.keys())
     for item in new_list:
         value = a_dictionary[item]
-        print(f'{item}:{value}')
+        print(f'{item}: {value}')
