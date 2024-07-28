00376285a128c0fe899a2ee171a31217844c4e3c
Maryam <morrryyyy21@gmail.com>
2024-07-28 11:50:30 +0100

    more corrections

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -36,12 +36,13 @@ class Square:
             raise TypeError("Size must be an integer")
         if value < 0:
             raise ValueError("Size must be >= 0")
+
         else:
             self.__size = value
+
     def area(self):
         """calculates the square's area
         Returns:
-            The area of the square
+        The area of the square
         """
         return (self.__size) ** 2
-
