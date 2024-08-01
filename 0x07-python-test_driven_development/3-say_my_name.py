e047dd8d5503e88f1b2e6313cdddea162504ec36
Maryam <morrryyyy21@gmail.com>
2024-08-01 19:14:58 +0100

    prints a string

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This is the 3-say_my_name module.
+It has only one function: say_my_name(first_name, last_name="")"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Prints the user's full name
+
+    Args:
+        first_name (string): the user's first name.
+        last_name (string): the user's last name.
+
+    Returns:
+        the user's full_name
+    """
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print(f"My name is {first_name} {last_name}")
