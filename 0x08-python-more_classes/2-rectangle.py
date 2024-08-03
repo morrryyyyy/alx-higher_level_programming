886e862fb2d9abba1cd0011e8d386224ece1f2b4
Maryam <morrryyyy21@gmail.com>
2024-08-03 11:21:53 +0100

    fix perimeter error

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -61,4 +61,6 @@ class Rectangle():
         Returns:
             the perimeter of the rectangle
         """
+        if self.__width == 0 or self.__height == 0:
+            return 0
         return 2 * (self.__width + self.__height)
