ffd15f0c3ea5fbe3bdaaa00b31e8b7e2d1d15b90
Maryam <morrryyyy21@gmail.com>
2024-08-03 10:10:58 +0100

    create a rectangle with a width and a height

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+This is the 2-rectangle module
+It contains the class rectangle
+"""
+
+
+class Rectangle():
+    """
+    Creates a class called Rectangle
+
+    """
+    def __init__(self, width=0, height=0):
+        """
+        Initializes a new rectangle
+
+        Args:
+            width (int): the width of the rectangle
+            height (int): the height of the rectangle
+        """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """Get/set the current width of the square"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get/set the current height of the square"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
