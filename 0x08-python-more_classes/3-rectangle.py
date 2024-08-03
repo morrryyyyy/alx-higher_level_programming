d6cd6043652d423ddcc634eea015478163fbd89d
Maryam <morrryyyy21@gmail.com>
2024-08-03 11:35:46 +0100

    pycodestyle validation

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -64,6 +64,7 @@ class Rectangle():
         if self.width == 0 or self.height == 0:
             return 0
         return 2 * (self.__width + self.__height)
+
     def __str__(self):
         if self.width == 0 or self.height == 0:
             return ""
