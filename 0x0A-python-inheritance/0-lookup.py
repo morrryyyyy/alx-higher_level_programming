ba62b1fc42091e6bb6e99fc033d8b1f6332927da
Maryam <morrryyyy21@gmail.com>
2024-08-05 11:44:12 +0100

    pycodestyle validation

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -9,7 +9,7 @@ It contains one function: lookup(obj)
 def lookup(obj):
     """
     Lists the methods and attributes of an object
-    
+
     Returns:
         the list of available attributes and methods of the object
     """
