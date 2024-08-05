d2dd09ea7c223c478d91c7a287bd477da3f77603
Maryam <morrryyyy21@gmail.com>
2024-08-05 14:21:40 +0100

    print an unsorted list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This is the 1-my_list module
+It contains only one class: MyList
+"""
+class MyList(list):
+    """A subclass of the list object"""
+    def print_sorted(self):
+        """
+        A public instance method
+        Prints the list in ascending order
+        """
+        new_list = sorted(self)
+        print(new_list)
