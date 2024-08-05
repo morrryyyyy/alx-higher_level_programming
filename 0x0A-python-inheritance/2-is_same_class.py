6b65b3e0324a2b10801db5148d109225c1a35c84
Maryam <morrryyyy21@gmail.com>
2024-08-05 14:31:31 +0100

    check if obj is an instance of a_class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This is the 2-is_same_class module
+It contains ne function: is_same_class(obj, a_class)
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Checks if an object is an instance of a class
+
+    Args:
+        obj (object): The object to check.
+        a_class (type): The class to check against.
+
+    Returns:
+        bool: True if obj is an instance of a_class, otherwise False.
+    """
+    if not isinstance(obj, a_class):
+        return False
+    return True
