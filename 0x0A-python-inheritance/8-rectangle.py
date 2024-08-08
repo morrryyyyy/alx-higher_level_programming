4f15e970771f4022040e76ca0f0bca284077d98c
Maryam <morrryyyy21@gmail.com>
2024-08-08 11:28:48 +0100

    done

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -4,7 +4,7 @@ This contains the BaseGeometry class
 """
 
 
-class BaseGeometry():
+class BaseGeometry:
     """
     A class with an unimplemented method
 
@@ -18,15 +18,15 @@ class BaseGeometry():
     def integer_validator(self, name, value):
         """
         Checks if value is an integer greater than 0
+
         Raises:
             TypeError: if value is not an integer
-            ValueError: if value is less than 0
+            ValueError: if value is less than or equal to 0
         """
-        self.name = name
         if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(self.name))
+            raise TypeError(f"{name} must be an integer")
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(self.name))
+            raise ValueError(f"{name} must be greater than 0")
 
 
 class Rectangle(BaseGeometry):
@@ -44,7 +44,7 @@ class Rectangle(BaseGeometry):
 
         Args:
             width (int): the width of the rectangle
-            height (int): the hieght of the rectangle
+            height (int): the height of the rectangle
         """
         super().__init__()
 
