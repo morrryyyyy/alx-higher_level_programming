ac30d00c7294f459e82c4553b382bb68f256be2d
Maryam <morrryyyy21@gmail.com>
2024-08-08 11:48:37 +0100

    str method

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+This is a square which inherits from the rectangle class
+"""
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """
+    A square class which inherits from the rectangle class
+    """
+    def __init__(self, size):
+        """
+        Initializes the square with a size.
+
+        Args:
+            size (int): the size of the square.
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """
+        Calculates the area of the square.
+
+        Returns:
+            int: the area of the square.
+        """
+        return self.__size ** 2
+
+    def __str__(self):
+        """informal string representation of the rectangle"""
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
