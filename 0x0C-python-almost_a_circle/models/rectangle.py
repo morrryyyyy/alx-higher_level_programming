cdef38283a4d0ce36fc61f80d7533f06de72f96e
Maryam <morrryyyy21@gmail.com>
2024-08-12 13:51:40 +0100

    correctly import base class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,7 +4,7 @@ This is the rectangle module.
 It contaisn the class Rectangle
 """
 
-Base = __import__("base").Base
+from models.base import Base
 
 
 class Rectangle(Base):
@@ -36,7 +36,7 @@ class Rectangle(Base):
         """Set the width of the rectangle"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value < 0:
+        if value <= 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -50,7 +50,7 @@ class Rectangle(Base):
         """Set the height of the rectangle"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if value < 0:
+        if value <= 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
