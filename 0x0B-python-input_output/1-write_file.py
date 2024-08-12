1562df10514a2ea71b6c07bd3f916ddf4e1927c4
Maryam <morrryyyy21@gmail.com>
2024-08-12 09:59:34 +0100

    pycdestyle validation

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -9,15 +9,15 @@ def write_file(filename="", text=""):
     """
     Overwrites the contents of a file.
     Create a new file if it doesn't exist.
-    
+
     Args:
         filename: the file to be overwritten.
         text: the text to add.
-    
+
     Returns:
         int: the number of characters added.
     """
 
     with open(filename, "w") as f:
-        number_of_chars =f.write(text)
+        number_of_chars = f.write(text)
     return number_of_chars
