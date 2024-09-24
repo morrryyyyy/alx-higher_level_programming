9f90370015c4c3954d7f1d8ac48d3b67a2905e1c
Maryam <morrryyyy21@gmail.com>
2024-09-24 14:25:50 +0100

    modify __str__ method for rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -91,6 +91,5 @@ class Rectangle(Base):
             print('#' * self.width)
 
     def __str__(self):
-        return (f"[Rectangle] ({self.id})",
-                f"{self.x}/{self.y} -",
-                f"{self.width}/{self.height}")
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id,self.x, self.y, self.width, self.height)
