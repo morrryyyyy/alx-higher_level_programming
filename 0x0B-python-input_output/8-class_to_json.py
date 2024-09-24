82758a61920eb400677fd53e90235cb56c543a23
Maryam <morrryyyy21@gmail.com>
2024-09-24 10:14:45 +0100

    return dict representation of a class

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This is the 8-class_to_json module.
+It contains the function: class_to_json(obj).
+"""
+
+
+def class_to_json(obj):
+    """
+    converts a class to its JSON representation.
+
+    Args:
+        obj(object): an instance of the class.
+
+    Returns:
+    the dictionary representation of the class.
+    """
+
+    def class_to_json(obj):
+        attributes = obj.__dict__
+        return attributes
