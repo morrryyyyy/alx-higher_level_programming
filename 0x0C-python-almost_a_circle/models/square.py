386636c458c9af51ec5e1a01531b8d42ebbe2b5f
Maryam <morrryyyy21@gmail.com>
2024-09-24 18:17:53 +0100

    edit str method for square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -18,4 +18,4 @@ class Square(Rectangle):
 
     def __str__(self):
         return "[Square] ({}) {}/{} - {}".format(
-            self.id, self.x, self.y, self.size)
+            self.id, self.x, self.y, self.width)
