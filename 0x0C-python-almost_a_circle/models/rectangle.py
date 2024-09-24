03fa8313872a3af43200c08ae479d3c47eaf086f
Maryam <morrryyyy21@gmail.com>
2024-09-24 14:18:09 +0100

    modify __str__ method for rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -89,3 +89,6 @@ class Rectangle(Base):
 
         for j in range(self.height):
             print('#' * self.width)
+
+    def __str__(self):
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
