ae626661d73c0cc36e457faae3278346d5c204bb
Maryam <morrryyyy21@gmail.com>
2024-09-24 14:26:31 +0100

    pycodestyle validation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -92,4 +92,4 @@ class Rectangle(Base):
 
     def __str__(self):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
-            self.id,self.x, self.y, self.width, self.height)
+            self.id, self.x, self.y, self.width, self.height)
