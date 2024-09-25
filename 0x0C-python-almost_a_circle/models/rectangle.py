6b0e71ed119493d403ffd134f6c453c685ccfa00
Maryam <morrryyyy21@gmail.com>
2024-09-25 19:45:07 +0100

    add dict methd to rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -121,3 +121,16 @@ class Rectangle(Base):
                     self.x = value
                 elif key == "y":
                     self.y = value
+
+    def to_dictionary(self):
+        """
+        returns the dictionary represntation of rectangle
+        """
+        my_dict = {
+            "id": self.id,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y
+        }
+        return my_dict
